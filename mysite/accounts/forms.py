a9c95d0078e2a8c21a1b4b584d08f5ff68a00a9d
tanver <tanver@gmail.com>
2024-01-08 20:00:42 +0600

    Final touches added

---
+++ mysite/accounts/forms.py
@@ -0,0 +1,21 @@
+from dataclasses import fields
+from importlib.metadata import files
+from pyexpat import model
+from django.forms import ModelForm,formset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import *
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+        exclude = ['user']
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
